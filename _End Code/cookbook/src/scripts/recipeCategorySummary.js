6275d663fb7449f8b5db6a292a9aa90c3fe99719
sreenathtv <sree.harishree@gmail.com>
2016-09-03 23:49:04 +1000

    Removed _End Code

--- _End Code/cookbook/src/scripts/recipeCategorySummary.js
+++
@@ -1,8 +0,0 @@
-var RecipeCategorySummary = (function () {
-    function RecipeCategorySummary(summary) {
-        this.text = summary.text;
-        this.title = summary.title;
-    }
-    return RecipeCategorySummary;
-}());
-//# sourceMappingURL=recipeCategorySummary.js.map
